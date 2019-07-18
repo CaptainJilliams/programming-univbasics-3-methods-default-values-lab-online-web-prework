883ca158d01ce1e600f248b0383eaa07cb97bf57
Andrew Williams <1williamsaj@gmail.com>
2019-07-18 02:32:35 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,8 +3,6 @@ def meal_choice(veg1, veg2, protein="meat")
   p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("peas", "carrots", "chicken")
-
 veg1 = "carrots"
 veg2 = "peas"
 protein = "chicken"
